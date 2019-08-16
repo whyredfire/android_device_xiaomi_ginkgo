220dc8393c446a6d01269f14ce217f0aba9ddfc9
Stylogey <stylogey@gmail.com>
2019-08-16 08:35:51 +0200

    ginkgo: parts: Implement vibration strength control
    [ghostrider-reborn]
    - massive code refactoring, improvements and cleanup
    
    [mg712702]
    - configure permissions and sepolicy
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Mrinal Ghosh <mg712702@gmail.com>

--- parts/Android.mk
+++ parts/Android.mk
@@ -22,6 +22,7 @@ LOCAL_USE_AAPT2 := true
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
     androidx.preference_preference \
+    me.waveproject.framework \
     SettingsLib
 
 LOCAL_RESOURCE_DIR := \
